22e84ac5e18223deb99d0c12004cc01781a2dff2
rigofelix <jesus.felix@fincamex.com.mx>
2018-08-27 17:32:01 -0600

    agrega soporte para cors

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ ADD requirements.txt /app/
 RUN pip install -r requirements.txt
 ADD . /app/
 EXPOSE 8000
+RUN python manage.py makemigrations
+RUN python manage.py migrate
 CMD ["python", "./manage.py", "runserver", "0.0.0.0:8000"]
--- todo/settings.py
+++ todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'apps.api',
     'apps.todos',
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'todo.urls'
@@ -121,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8000',
+    '127.0.0.1:8000',
+    'localhost:8080',
+    '127.0.0.1:8080'
+)
